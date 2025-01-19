6036cfa5722abfc647974ab3ebdc94f2438f4e0d
Krishna <80216446+PhyG0@users.noreply.github.com>
2025-01-19 22:42:09 +0530

    Add files via upload

---
+++ pg/oiiai/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$app_name = "Webler Group";
+
+$dbHost = 'localhost';
+$dbName = 'webler_localhost_db';
+$dbDSN = "mysql:host=$dbHost;dbname=$dbName;";
+$dbUser = 'webler';
+$dbPassword = 'password';
+
+?>
